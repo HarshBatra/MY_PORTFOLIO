6f9198b0d08510a8d73ace2a64957013cfa93cba
HARSH BATRA <harshbat22@gmail.com>
2024-01-31 01:32:47 +0530

    added header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,46 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="relative mb-40">
+      <img
+        alt="mandala"
+        className="w-full opacity-20 absolute -z-10"
+        src={require("../assets/images/mandala.png")}
+      />
+      <div className="flex md:flex-row flex-col-reverse justify-center md:justify-around py-20">
+        <div className="py-10 self-center">
+          <div className="text-blue-950 text-2xl md:text-5xl font-bold text-center md:text-left">
+            Hello!
+          </div>
+          <div className="text-blue-900 text-xl md:text-3xl font-bold text-center md:text-left">
+            I'm Harsh Batra!
+          </div>
+          <div className="my-10">
+            <div className="px-4 py-2 bg-white drop-shadow-lg text-blue-950 rounded-xl m-4 text-center font-semibold">
+              Software Developer
+            </div>
+            <div className="px-4 py-2 bg-white drop-shadow-lg text-blue-950 rounded-xl m-4 text-center font-semibold">
+              Programming Enthusiast
+            </div>
+            <div className="px-4 py-2 bg-white drop-shadow-lg text-blue-950 rounded-xl m-4 text-center font-semibold">
+              MERN Stack Web Developer
+            </div>
+            <div className="px-4 py-2 bg-white drop-shadow-lg text-blue-950 rounded-xl m-4 text-center font-semibold">
+              UI/UX Designer
+            </div>
+          </div>
+        </div>
+        <div className="bg-blue-100 border-8 border-white drop-shadow-2xl h-fit w-1/2 md:w-1/4 self-center">
+          <img
+            alt="Harsh"
+            className="border-8 border-white drop-shadow-2xl rotate-6"
+            src={require("../assets/images/photo.jpg")}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,20 +6,20 @@ import { SiLeetcode } from "react-icons/si";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-around bg-blue-100 py-4 items-center drop-shadow-xl">
-      <div className="font-semibold text-lg text-blue-950">Harsh Batra</div>
+    <div className="flex justify-around bg-blue-50 py-4 items-center drop-shadow-xl">
+      <div className="font-semibold md:text-xl text-blue-950">Harsh Batra</div>
       <div className="flex gap-2 md:gap-4 text-blue-950">
         <a href="https://twitter.com/harshbatra22">
-          <BsTwitterX className="w-5 h-5 cursor-pointer hover:text-white" />
+          <BsTwitterX className="w-5 h-5 cursor-pointer drop-shadow-md hover:text-white" />
         </a>
         <a href="https://linkedin.com/in/harsh-batra-b793951ab">
-          <FaLinkedin className="w-5 h-5 cursor-pointer hover:text-white" />
+          <FaLinkedin className="w-5 h-5 cursor-pointer drop-shadow-md hover:text-white" />
         </a>
         <a href="https://github.com/HarshBatra">
-          <FaGithub className="w-5 h-5 cursor-pointer hover:text-white" />
+          <FaGithub className="w-5 h-5 cursor-pointer drop-shadow-md hover:text-white" />
         </a>
         <a href="https://leetcode.com/Batraaaaaa">
-          <SiLeetcode className="w-5 h-5 cursor-pointer hover:text-white" />
+          <SiLeetcode className="w-5 h-5 cursor-pointer drop-shadow-md hover:text-white" />
         </a>
       </div>
       <a href="https://drive.google.com/file/d/1eUIsXnATVw1GJ4OpELz4V2cKNOoshv9u/view">
